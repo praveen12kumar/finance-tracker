8e145d9e21815f4844473b7b2bc2d04aed675972
praveen kumar <praveenshakya1@gmail.com>
2023-12-12 13:03:47 +0530

    add helmet

--- backend/App.js
+++ backend/App.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {config} from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 import {errorHandler} from "./middlewares/errorHandler.js";
 import IncomeRouter from "./router/IncomeRouter.js";
 import ExpenseRouter from "./router/ExpenseRouter.js";
@@ -8,11 +10,11 @@ import SavingRouter from "./router/SavingRouter.js";
 config({path:"backend/config/config.env"})
 const app = express();
 
-
-
 // using middleware 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 
 //Routers
